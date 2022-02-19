4434d49cc3a013ba40c60382769047c59ceb3727
KashtakDev <60832747+EvgenyZaev@users.noreply.github.com>
2022-02-19 14:17:45 -0500

    Add project files.

---
+++ part1/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using part1.Models;
+using System.Diagnostics;
+
+
+namespace part1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            //
+
+            if (Request.Cookies["cook"] == null)
+            {
+                CookieOptions options = new CookieOptions();
+                options.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Append("cook", "2");
+
+                this.ViewData["visits_times"] = Request.Cookies["cook"];
+            } 
+            else
+            {
+                
+                var cook = Int32.Parse(Request.Cookies["cook"]);
+                cook++;
+
+                CookieOptions options = new CookieOptions();
+                options.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Append("cook", cook.ToString());
+
+
+                this.ViewData["visits_times"] = Request.Cookies["cook"];
+                this.ViewData["ip_addr"] = remoteIpAddress;
+
+            }
+
+
+            
+
+            return View();
+        }
+
+     
+
+    }
+}
---
+++ part1/Models/Visitor.cs
@@ -0,0 +1,7 @@
+﻿namespace part1.Models
+{
+    public class Visitor
+    {
+        public int MyProperty { get; set; }
+    }
+}
